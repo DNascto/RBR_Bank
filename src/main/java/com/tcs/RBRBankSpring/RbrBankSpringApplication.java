00202078b6c2f6672a9a9a3995190a3226bbd9b9
Daniel Nascimento <1744966@TCS.com>
2019-09-12 14:28:30 -0300

    first

---
+++ src/main/java/com/tcs/RBRBankSpring/RbrBankSpringApplication.java
@@ -0,0 +1,13 @@
+package com.tcs.RBRBankSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RbrBankSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RbrBankSpringApplication.class, args);
+	}
+
+}
