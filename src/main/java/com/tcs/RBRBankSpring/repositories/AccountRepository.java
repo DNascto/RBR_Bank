d91462ba73c868d12256ba2e3d60dbf7374f2d0e
1744965 <cesarcardoso954@gmail.com>
2019-09-13 12:19:13 -0300

    Erro arrumado

--- src/main/java/com/tcs/RBRBankSpring/repositories/AccountRepository.java
+++ src/main/java/com/tcs/RBRBankSpring/repositories/AccountRepository.java
@@ -1,8 +1,9 @@
 package com.tcs.RBRBankSpring.repositories;
 
+import com.tcs.RBRBankSpring.models.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AccountRepository extends JpaRepository<AccountRepository, Long> {
+public interface AccountRepository extends JpaRepository<Account, Long> {
 }
--- src/main/java/com/tcs/RBRBankSpring/repositories/InvestmentRepository.java
+++ src/main/java/com/tcs/RBRBankSpring/repositories/InvestmentRepository.java
@@ -1,8 +1,9 @@
 package com.tcs.RBRBankSpring.repositories;
 
+import com.tcs.RBRBankSpring.models.Investment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface InvestmentRepository extends JpaRepository<InvestmentRepository, Long> {
+public interface InvestmentRepository extends JpaRepository<Investment, Long> {
 }
