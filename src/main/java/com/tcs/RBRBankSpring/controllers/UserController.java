382dac5b8237b6497559945303a0bc77ec25db22
AndersonSutil <andersonsutil.off@gmail.com>
2019-09-16 17:34:23 -0300

    Revisão Erros na tranferencia do json

--- src/main/java/com/tcs/RBRBankSpring/controllers/UserController.java
+++ src/main/java/com/tcs/RBRBankSpring/controllers/UserController.java
@@ -5,6 +5,7 @@ import com.tcs.RBRBankSpring.models.User;
 import com.tcs.RBRBankSpring.repositories.UserRepository;
 import com.tcs.RBRBankSpring.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,7 +13,7 @@ import javax.persistence.Transient;
 
 @RestController
 @RequestMapping("/rbr/user")
-@CrossOrigin(origins = "http://localhost:4200")
+//@CrossOrigin(origins = "http://localhost:4200")
 public class UserController {
 
     @Autowired
@@ -29,10 +30,11 @@ public class UserController {
         return ResponseEntity.ok(userService.checkUserExistence(numberAccount));
     }
 
-    @PostMapping("/login")
-    public User loginAccount(@RequestBody String login, @RequestBody String password){
-        return userService.validateLogin(login, password);
+    @PostMapping(value = "/login",produces = MediaType.TEXT_PLAIN_VALUE)
+    public String index(){
+        return "this is login home";
     }
+
 
     @PostMapping("/transfer")
     public boolean doTransfer(@RequestBody User user, @RequestBody User addressee, @RequestBody Double value){
--- src/main/java/com/tcs/RBRBankSpring/services/UserService.java
+++ src/main/java/com/tcs/RBRBankSpring/services/UserService.java
@@ -12,7 +12,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 @Component
-public class UserService {
+public class UserService  {
     @Autowired
     UserRepository userRepository;
 
